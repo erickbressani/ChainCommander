35adba200b1bd3e61f0e6c4b720fdb5d45150332
Erick Bressani <Erick.Bressani@farfetch.com>
2020-07-01 10:52:08 +0100

    Remove unused namespace

--- src/Sample/Console/Program.cs
+++ src/Sample/Console/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Diagnostics.CodeAnalysis;
+﻿using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 using ChainCommander.Sample.Implementation;
 using Microsoft.Extensions.DependencyInjection;
